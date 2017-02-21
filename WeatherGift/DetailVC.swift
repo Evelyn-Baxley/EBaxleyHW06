31e8162f4bc3c60a1d0268c4b1ea7e8ff2763dee
CSOM <CSOM@bc-macbookpro-4a3e61.local>
2017-02-21 09:41:53 -0500

    7.2 Interface Items

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by CSOM on 2/21/17.
+//  Copyright © 2017 CSOM. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
